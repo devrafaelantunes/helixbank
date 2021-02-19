c61c5c173d871607a20028eaefe04386c064cf5d
Rafael <dev@rafaelantun.es>
2021-02-19 18:59:37 -0300

    second commit

--- lib/helixbank/internal/account.ex
+++ lib/helixbank/internal/account.ex
@@ -1,16 +1,101 @@
 defmodule HelixBank.Internal.Account do
-    import Ecto.Query
+    import Ecto.{Query, Changeset}
     alias HelixBank.Model.Account
     alias Helixbank.Repo
 
 
-    def get_account_number() do
-        acc_number = Enum.random(2222..9999)
+    def create_account(params) do
+        account_number = generate_account_number()
+        
+        new_params =
+            %{account_number: account_number, agency_id: 0001, amount: 0}
+            |> Map.merge(params)
 
-        if Repo.get_by(Account, account_number: acc_number) do
-            acc_number
+        new_params
+        |> Account.create_changeset()
+        |> Repo.insert
+
+    end
+
+    def deposit(account_number, amount) do
+        if account_exists?(account_number) do
+            old_value = 
+                account_number
+                |> get_amount_from_account() ## tiurar isso fazer um proprio e colocar select
+            
+            fetch_account_by_number(account_number)
+            |> Repo.one()
+            |> Account.update_amount(old_value + amount)
+            |> Repo.update()
+        
+        
         else
-            get_account_number()
+            raise "the account number does not exist"
         end
     end
+
+    def transfer(account_number, to_account, amount) do
+        if account_exists?(account_number) and account_exists?(to_account) do
+            if get_amount_from_account(account_number) >= amount do
+                withdraw(account_number, amount)
+                deposit(to_account, amount)
+            else
+                raise "you have no funds"
+            end
+
+        else
+            raise "the account does not exist"
+        end
+
+
+    end 
+
+    def withdraw(account_number, amount) do
+        if account_exists?(account_number) do
+            old_value = 
+                account_number
+                |> get_amount_from_account()
+
+            fetch_account_by_number(account_number)
+            |> Repo.one()
+            |> Account.update_amount(old_value - amount)
+            |> Repo.update()
+        else
+            raise "the account number does not exist"
+
+        end
+
+    
+    end
+
+    defp get_amount_from_account(account_number) do
+        query = 
+            from(a in Account,
+                where: a.account_number == ^account_number,
+                select: a.amount)
+
+        Repo.one(query)
+
+    end
+
+    defp generate_account_number() do
+        acc_number = Enum.random(2222..9999)
+
+        if account_exists?(acc_number) do
+            generate_account_number()
+        else
+            acc_number
+        end
+    end
+
+    defp account_exists?(acc_number) when is_integer(acc_number) do
+        fetch_account_by_number(acc_number)
+        |> Repo.exists?()
+    end
+
+    defp fetch_account_by_number(acc_number) do
+        from(a in Account, 
+            where: a.account_number == ^acc_number)
+    end
+
 end
--- lib/helixbank/model/account.ex
+++ lib/helixbank/model/account.ex
@@ -1,30 +1,46 @@
 defmodule HelixBank.Model.Account do
     use Ecto.Schema
-    
+    import Ecto.Changeset
+
     alias HelixBank.Internal.Account, as: Internal
 
     @primary_key {:account_id, :id, autogenerate: true}
     schema "account" do
         field :name, :string
         field :document, :string
-        field :amount, :float, default: 0
-        field :agency_id, :integer, default: 0001
-        field :account_number, :integer, default: Internal.get_account_number()
+        field :amount, :float
+        field :agency_id, :integer
+        field :account_number, :integer
 
         timestamps()
     end
 
-    #def changeset(params) do
-    #    %__MODULE__{}
-#
-#
-#
- #   end
+    def create_changeset(params) do
+        %__MODULE__{}
+        |> cast(params, [:name, :document, :amount, :agency_id, :account_number])
+        |> validate_required([:name, :document, :agency_id, :account_number])
+        |> unique_constraint(:document)
+        |> validate_length(:document, min: 11)
+        |> validate_document(params.document)
+    end
 
+    defp validate_document(changeset, param) do
+        valid_sum = 
+            [22,33,44,55,66,77,88,99]
 
+        valid_param =
+            String.to_integer(param)
+            |> Integer.digits()
+            |> Enum.sum()
 
+        if Enum.member?(valid_sum, valid_param) do
+            changeset
+        else
+            add_error(changeset, :document, "the document is not valid")
+        end
+    end
 
-
-
-
+    def update_amount(account, new_value) do
+        change(account, amount: new_value)
+    end
 end
